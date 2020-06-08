5d8b61d5857c8c26a2c749953c172bb7cedf8e41
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-06-08 18:42:53 +0900

    delete some data.

--- lib/generic_utils.R
+++
@@ -1,9 +0,0 @@
-# Author iwasakishuto
-# Date 2020-06-07
-
-# Install `probelms`, `spec`, ...
-library(readr)
-
-print_save_log <- function(path){
-    print(paste("Save at ", path, "", sep="'"))
-}
