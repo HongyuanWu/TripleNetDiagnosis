170c1fa2f5f618bb53e66900f23e88c669414f23
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-06-08 15:49:43 +0900

    Fixed some bugs.

--- lib/GEO_utils.R
+++ lib/GEO_utils.R
@@ -8,7 +8,7 @@ library(GEOquery)
 library(limma)
 
 GSE2MArrayLM <- function(GSE=NULL, filename=NULL, GPL=NULL, destdir="~/.GEO",
-                         colname="", ctrls=c(), cases=c()){
+                         colname="", ctrls=c(), cases=c(), all=TRUE){
   if (FALSE){
     paste("Create a MArrayLM from GSE names.",
           "@reference       : https://www.ncbi.nlm.nih.gov/geo/geo2r",
@@ -19,13 +19,20 @@ GSE2MArrayLM <- function(GSE=NULL, filename=NULL, GPL=NULL, destdir="~/.GEO",
           "@params destdir  : The destination directory for any downloads.",
           "@params ctrls    : A character vector which represent belonging to `control` samples.",
           "@params cases    : A character vector which represent belonging to `case` samples.",
-          "@params colnames : A character string representing a column name where we can distinguish `cases` and `controls`.")
+          "@params colnames : A character string representing a column name where we can distinguish `cases` and `controls`.",
+          "@params all      : A boolean whether you extract all columns or only important ones.")
   }
   if ((length(ctrls)==0 && length(cases)==0) || length(colname)==0){
     print(paste0("Please visit GEO2R (https://www.ncbi.nlm.nih.gov/geo/geo2r) to ",
                  "check how to distinguish 'cases' and 'controls', and define them ",
                  "using `ctrls`, `cases`, `colname` arguments."))
     return (NULL)
+  } else {
+    ctrls <- unlist(ctrls)
+    cases <- unlist(cases)
+  }
+  if (dir.exists(destdir)==FALSE){
+    dir.create(destdir, recursive=TRUE)
   }
   if (is.null(filename)) GSE<-GSE else GSE<-NULL
   # This function is the main user-level function in the GEOquery package.
@@ -33,7 +40,7 @@ GSE2MArrayLM <- function(GSE=NULL, filename=NULL, GPL=NULL, destdir="~/.GEO",
   # SOFT format file into an R data structure specifically designed to make
   # access to each of the important parts of the GEO SOFT format easily accessible.
   gset <- getGEO(GEO=GSE, filename=filename, destdir=destdir, GSEMatrix=TRUE, AnnotGPL=TRUE)
-  if (length(gset)>1 && is.null(GPL)==FALSE) idx<-grep(GPL, attr(gset, "names")) else idx<-1
+  if (length(gset)>1 && is.null(GPL)==FALSE) idx<-grep(pattern=GPL, x=attr(gset, "names")) else idx<-1
   gset <- gset[[idx]]
 
   # Make syntactically valid names out of character vectors.
@@ -42,8 +49,9 @@ GSE2MArrayLM <- function(GSE=NULL, filename=NULL, GPL=NULL, destdir="~/.GEO",
 
   # Extract "control" and "case" samples.
   j <- 1
+  colname <- tolower(colname)
   for (label in varLabels(gset)) {
-      if (any(grep(colname, label))){ break }
+      if (any(grep(pattern=colname, x=label))){ break }
       j <- j + 1
   }
   group_cols <- gset[[j]]
@@ -52,18 +60,18 @@ GSE2MArrayLM <- function(GSE=NULL, filename=NULL, GPL=NULL, destdir="~/.GEO",
     group <- group_cols[i]
     sml[i] <- "X"
     for (ctrl in ctrls){
-      if (any(grep(ctrl, group))){
+      if (any(grep(pattern=ctrl, x=group))){
         sml[i] <- "0"
       }
     }
     for (case in cases){
-      if (any(grep(case, group))){
+      if (any(grep(pattern=case, x=group))){
         sml[i] <- "1"
       }
     }
   }
-  print(paste(sum(sml=="1"), "control samples."))
-  print(paste(sum(sml=="0"), "case samples."))
+  print(paste(sum(sml=="0"), "control samples."))
+  print(paste(sum(sml=="1"), "case samples."))
   print(paste(sum(sml=="X"), "other samples."))
   # eliminate samples marked as "X"
   sel  <- which(sml != "X")
@@ -92,6 +100,17 @@ GSE2MArrayLM <- function(GSE=NULL, filename=NULL, GPL=NULL, destdir="~/.GEO",
   fit2 <- contrasts.fit(fit, cont.matrix)
   fit2 <- eBayes(fit2, 0.01)
   tT <- topTable(fit2, adjust="fdr", sort.by="B", number=250)
-  tT <- subset(tT, select=c("ID","adj.P.Val","P.Value","t","B","logFC","Gene.symbol","Gene.title"))
+  if (all==FALSE){
+    important_cols <- c("ID","adj.P.Val","P.Value","t","B","logFC","Gene.symbol","Gene.title")
+    extract_cols   <- character()
+    for (col in important_cols){
+      if (col %in% colnames(gset)){
+        extract_cols <- c(extract_cols, col)
+      }
+    }
+    tT <- subset(tT, select=extract_cols)
+    print("Extract only the following columns.")
+    print(paste(extract_cols, collapse=", "))
+  }
   return (tT)
 }
